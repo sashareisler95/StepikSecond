1233ba46562518eb522bb12b8ff224e8c76358e5
Sasha <aleksandra.reisler@gmail.com>
2022-01-10 17:59:47 +0600

    debug

--- conftest.py
+++ conftest.py
@@ -3,19 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-def default_lang(parser):
-    parser.addoption('--accept_languages', action='store', default="ru",
-                     help="Choose language: ")
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language", action="store", default="ru", help="Choose lang"
+    )
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_lang = request.config.getoption("lang")
-    print("\nstart browser for test..")
+    browser_lang = request.config.getoption("--language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': browser_lang})
     options.add_argument("--window-size=1920x1080")
     browser = webdriver.Chrome(options=options)
     yield browser
-    print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,12 +1,8 @@
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
-link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 def test_button(browser):
     browser.get(link)
-    try:
-        browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
-    except NoSuchElementException:
-        print('I do not found button!')
+    assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket"), "I do not found button!"
